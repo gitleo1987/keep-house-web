33a7d0a34d514c75574e9dd5ffdee5a11064491c
leo caporicci <leito87@gmail.com>
2020-07-10 00:57:24 -0300

    Optimizaciones para PageSpeed

--- web/index.php
+++ web/index.php
@@ -5,22 +5,23 @@
 	html5up.net | @ajlkn
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 -->
-<html>
+<html lang="es">
 	<head>
 		<title>Keep House</title>
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+        <meta name="Description" content="KeepHouse - Cuidado y mantenimiento de propiedades.">
         <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
         <link rel="icon" href="favicon.ico" type="image/x-icon">
 		<link rel="stylesheet" href="assets/css/main.css" />
         <link rel="stylesheet" href="assets/css/keep.css" />
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0/jquery.min.js"></script>
-        <!-- jQuery Modal -->
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-modal/0.9.1/jquery.modal.min.js"></script>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery-modal/0.9.1/jquery.modal.min.css" />
         <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-beta.1/dist/css/select2.min.css" rel="stylesheet" />
 	</head>
     	<body class="is-preload">
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0/jquery.min.js"></script>
+            <!-- jQuery Modal -->
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-modal/0.9.1/jquery.modal.min.js"></script>
             <?php
             $mensaje = "";
             if(isset($_SESSION['mensaje']))
@@ -37,13 +38,13 @@
 		<!-- Header -->
 			<div id="header">
                 <div class="header-logo">
-				    <img class= "img-responsive logotipo" id="logokeep" src="images/logo.png">
+				    <img class= "img-responsive logotipo" id="logokeep" src="images/logo.png" alt="logo">
                 </div>
                 <div class="logo-vertical">
-                <img class="img-responsive" id= "logocel" src= "images/logo-vertical2.png">
+                <img class="img-responsive" id= "logocel" src= "images/logo-vertical2.png" alt="logo">
                 </div>
-                <div class="header-slogan"><img class="img-responsive" id="slogan" src="images/slogan.png"></div>
-                <div class="header-consulta"><a href="https://wa.me/5492921408809?text=Me%20interesa%20saber%20más%20del%20servicio" target="_blank"><img class="tel img-responsive" id="telefono" src="images/consulta.png"></a></div>
+                <div class="header-slogan"><img class="img-responsive" id="slogan" src="images/slogan.png" alt="slogan"></div>
+                <div class="header-consulta"><a href="https://wa.me/5492921408809?text=Me%20interesa%20saber%20más%20del%20servicio" target="_blank"><img class="tel img-responsive" id="telefono" src="images/consulta.png" alt="consulta"></a></div>
 			</div>
 
 		<!-- Main -->
@@ -62,10 +63,10 @@
                 <!-- Servicio Básico -->
 				<div class="box alt container">
 					<section class="feature left">
-						<div class="image icon fas keep-letra-b"> <img src="images/serv-basico.png" alt="" /></div>
+						<div class="image icon fas keep-letra-b"> <img src="images/serv-basico.jpg" alt="basico" /></div>
 						<div class="content">
-                            <a id="wssp" style="display: scroll;border:none; width: 50px; height: 50px; position: fixed; bottom: 50px; right: 20px; z-index: 2000;" title="Contactanos por Whatsapp" href="https://wa.me/5492921408809?text=Me%20interesa%20saber%20más%20del%20servicio" target="_blank"><img class="img-responsive" src="images/whatsapp.png"></a>
-                            <div id="logoflot" style="display: scroll;border:none; width: 50px; height: 50px; position: fixed; top: 50px; left: 20px; z-index: 2000;"><img class="img-responsive" src="images/logo-flotante.png"></div>
+                            <a id="wssp" style="display: scroll;border:none; width: 50px; height: 50px; position: fixed; bottom: 50px; right: 20px; z-index: 2000;" title="Contactanos por Whatsapp" href="https://wa.me/5492921408809?text=Me%20interesa%20saber%20más%20del%20servicio" target="_blank"><img class="img-responsive" src="images/whatsapp.png" alt="whatsapp"></a>
+                            <div id="logoflot" style="display: scroll;border:none; width: 50px; height: 50px; position: fixed; top: 50px; left: 20px; z-index: 2000;"><img class="img-responsive" src="images/logo-flotante.png" alt="logo-flotante"></div>
 							<h3>Servicio Básico</h3>
                             <p>   Visita quincenal con revisión de:</p>
                             <ul class="keep-lista-servicios">
@@ -102,7 +103,7 @@
 					<section class="feature right">
 						<div class="image icon fas keep-check imagen-no-seleccionada">
                             <div id="GA" class="service-selector" onclick="seleccionarServicio('GA')"></div>
-                            <img src="images/serv-averias.jpg" alt="" />
+                            <img src="images/serv-averias.jpg" alt="averias" />
                         </div>
 						<div class="content">
 							<h3>Gestión de averías</h3>
@@ -115,7 +116,7 @@
 					<section class="feature left">
                         <div class="image icon fas keep-check imagen-no-seleccionada">
                             <div id="CL" class="service-selector" onclick="seleccionarServicio('CL')"></div>
-                            <img src="images/serv-casa-lista.jpg" alt="" />
+                            <img src="images/serv-casa-lista.jpg" alt="casa-lista" />
                         </div>
 						<div class="content">
 							<h3>Casa Lista</h3>
@@ -131,7 +132,7 @@
 					<section class="feature right">
                         <div class="image icon fas keep-check imagen-no-seleccionada">
                             <div id="RC" class="service-selector" onclick="seleccionarServicio('RC')"></div>
-                            <img src="images/serv-representacion.jpg" alt="" />
+                            <img src="images/serv-representacion.jpg" alt="representacion" />
                         </div>
 						<div class="content">
 							<h3>Representación ante el consorcio</h3>
@@ -142,7 +143,7 @@
 					<section class="feature left">
                         <div class="image icon fas keep-check imagen-no-seleccionada">
                             <div id="PP" class="service-selector" onclick="seleccionarServicio('PP')"></div>
-                            <img src="images/serv-gestion-pago.jpg" alt="" />
+                            <img src="images/serv-gestion-pago.jpg" alt="gestion-pago" />
                         </div>
 						<div class="content">
 							<h3>Gestión y pago de proveedores</h3>
@@ -153,7 +154,7 @@
 					<section class="feature right">
                         <div class="image icon fas keep-check imagen-no-seleccionada">
                             <div id="RV" class="service-selector" onclick="seleccionarServicio('RV')"></div>
-                            <img src="images/serv-reformas.jpg" alt="" />
+                            <img src="images/serv-reformas.jpg" alt="reformas" />
                         </div>
 						<div class="content">
 							<h3>Reformas en la Vivienda</h3>
@@ -164,7 +165,7 @@
 					<section class="feature left">
                         <div class="image icon fas keep-check imagen-no-seleccionada">
                             <div id="TA" class="service-selector" onclick="seleccionarServicio('TA')"></div>
-                            <img src="images/serv-transfer.jpg" alt="" />
+                            <img src="images/serv-transfer.jpg" alt="transfer" />
                         </div>
 						<div class="content">
 							<h3>Transfer desde el Aeropuerto <br>o Bahía Blanca</h3>
@@ -176,7 +177,7 @@
 					<section class="feature right">
                         <div class="image icon fas keep-check imagen-no-seleccionada">
                             <div id="ER" class="service-selector" onclick="seleccionarServicio('ER')"></div>
-                            <img src="images/serv-envio-recep.jpg" alt="" />
+                            <img src="images/serv-envio-recep.jpg" alt="envio-recepcion" />
                         </div>
 						<div class="content">
 							<h3>Envío / Recepción</h3>
@@ -190,7 +191,7 @@
 					<section class="feature left">
                         <div class="image icon fas keep-check imagen-no-seleccionada">
                             <div id="LC" class="service-selector" onclick="seleccionarServicio('LC')"></div>
-                            <img src="images/serv-lena-carbon.jpg" alt="" />
+                            <img src="images/serv-lena-carbon.jpg" alt="carbon-leña" />
                         </div>
 						<div class="content">
 							<h3>Leña y Carbón</h3>
@@ -202,7 +203,7 @@
                     <section class="feature right">
                         <div class="image icon fas keep-check imagen-no-seleccionada">
                             <div id="SA" class="service-selector" onclick="seleccionarServicio('SA')"></div>
-                            <img src="images/serv-antiplagas.jpg" alt="" />
+                            <img src="images/serv-antiplagas.jpg" alt="antiplagas" />
                         </div>
 						<div class="content">
 							<h3>Servicio antiplagas</h3>
@@ -211,32 +212,6 @@
                         </div>
                     </section>
                 </div>
-				<!--
-				<div class="box container">
-
-					<section>
-						<header>
-							<h3>Form</h3>
-						</header>
-						<form method="post" action="#">
-							<div class="row">
-								<div class="col-12">
-									<label for="subject">Message</label>
-									<textarea name="message" id="message" placeholder="Enter your message" rows="6"></textarea>
-								</div>
-								<div class="col-12">
-									<ul class="actions special">
-										<li><input type="submit" value="Send" /></li>
-										<li><input type="reset" value="Reset" class="alt" /></li>
-									</ul>
-								</div>
-							</div>
-						</form>
-					</section>
-				</div>
-
-
-            -->
 			</div>
 
 		<!-- Footer -->
@@ -306,8 +281,8 @@
 							</div>
 						</div>
 					</form>
-                    <div class="map-responsive">
-                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d12406.09133193535!2d-61.313595455310335!3d-38.980562342553924!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x958d5290cbca6087%3A0x6250958d52aefc8a!2sMonte%20Hermoso%2C%20Provincia%20de%20Buenos%20Aires!5e0!3m2!1ses!2sar!4v1592172069662!5m2!1ses!2sar" width="800" height="600" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+                    <div class="mapa">
+                        <a href="https://goo.gl/maps/mmrK2eUGrD4xKCRPA" target="_blank"><img src="https://api.mapbox.com/styles/v1/mapbox/streets-v11/static/-61.3,-38.982,14,0/800x600?access_token=pk.eyJ1IjoibGVvY2Fwb3JpY2NpIiwiYSI6ImNrY2ZsZ3N4NjAyYzUzMXM4cTdramZ2aDAifQ.LC0427arsb0hP1pUy26JXA&attribution=false&logo=false" alt="Mapa de Monte Hermoso"></a>
                     </div>
                     <br>
                     <ul>
@@ -335,7 +310,7 @@
 			<script src="assets/js/jquery.min.js"></script>
 			<script src="assets/js/browser.min.js"></script>
 			<script src="assets/js/breakpoints.min.js"></script>
-            <script src="assets/js/jquery.inputmask.js"></script>
+            <script src="assets/js/jquery.inputmask.min.js"></script>
             <script src="assets/js/bindings/inputmask.binding.js"></script>
 			<script src="assets/js/util.js"></script>
 			<script src="assets/js/main.js"></script>
